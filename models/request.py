fcf95660c475eb5314df50f462779b95b37c8132
casperrobieniek6-ops <casperrobieniek6@gmail.com>
2026-04-23 15:43:13 -0400

    More File creation:

--- models/request.py
+++ models/request.py
@@ -1,1 +1,1 @@
-
+class Request:
--- services/CSVreader_service.py
+++ services/CSVreader_service.py
@@ -1,12 +1,15 @@
 import csv
-
+from data import *
 #Class for CSV Reader
 class CSVreader_service:
+    table = ['request_id', 'request_type', 'requester_name', 'location', 'urgency_level', 'estimated_cost', 'status',
+             'issue_type', 'days_open', 'attendees', 'event_date', 'hazard_level', 'response_time_minutes']
+
     def __init__(self):
         pass
-    #Method for opening file and converting to dic
-    def read_file(self, file_name):
-        with open(file_name) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            for row in csv_reader:
-            
+        def read_file(self, file_name):
+            with open(file_name) as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                table = []
+                for row in csv_reader:
+                    table.append(row)
