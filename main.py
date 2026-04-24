b51cb12f3da9511c67949e2f017e1acbc157145b
casperrobieniek6-ops <casperrobieniek6@gmail.com>
2026-04-24 17:47:05 -0400

    Convert data to rows and print: Ready for subclasses

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
+from models.request import Request
 from services.CSVreader_service import CSVReaderService
 
+def row_to_request(row):
+    return Request(
+        row['request_id'],
+        row['requester_name'],
+        row['location'],
+        row['urgency_level'],
+        row['estimated_cost'],
+        row['status'],
+        row['issue_type'],
+        row['days_open'],
+        row['attendees'],
+        row['event_date'],
+        row['hazard_level'],
+        row['response_time_minutes'])
 
 def main():
      csv_reader = CSVReaderService("data/service_requests.csv")
-     requests = csv_reader.get_requests()
+     rows = csv_reader.get_requests()
+
+     requests = []
+
+     for row in rows:
+         request = row_to_request(row)
+         requests.append(request)
 
      for request in requests:
-         print(request)
+         print(request.display_request())
 
 if __name__ == '__main__':
     main()
--- models/request.py
+++ models/request.py
@@ -1,7 +1,25 @@
-from services.CSVreader_service import
+from services.CSVreader_service import CSVReaderService
 class Request:
-    def __init__(self):
-        self.table = []
+    def __init__(self, request_id, requester_name, location, urgency_level, estimated_cost, status, issue_type, days_open, attendees, event_date, hazard_level, response_time_minutes):
+        self.request_id = request_id
+        self.requester_name = requester_name
+        self.location = location
+        self.urgency_level = urgency_level
+        self.estimated_cost = estimated_cost
+        self.status = status
+        self.issue_type = issue_type
+        self.days_open = days_open
+        self.attendees = attendees
+        self.event_date = event_date
+        self.hazard_level = hazard_level
+        self.response_time_minutes = response_time_minutes
 
-    def add_data(self, data):
-        self.table.append(data)
+    def display_request(self):
+        return {
+            f"ID: {self.request_id} | "
+            f"Requester: {self.requester_name} | "
+            f"Location: {self.location} | "
+            f"Urgency: {self.urgency_level} | "
+            f"Cost: {self.estimated_cost} | "
+            f"Status: {self.status}"
+        }
